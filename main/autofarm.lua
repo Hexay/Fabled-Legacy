70e16fbe61d9366c7fdaf111825d99952400d808
Hexay <97907668+Hexay@users.noreply.github.com>
2023-04-15 12:00:28 +0100

    Update autofarm.lua

--- main/autofarm.lua
+++ main/autofarm.lua
@@ -2,8 +2,8 @@
 getgenv().Map = "Raided Village"
 getgenv().Hardcore = false --If true then bot won't suicide
 getgenv().Private = true
-getgenv().Difficulty = "Chaos"
-getgenv().LevelRequirement = 15 --Minimun level, this must be set correctly else invalid dungeon
+getgenv().Difficulty = "Expert"
+getgenv().LevelRequirement = 5 --Minimun level, this must be set correctly else invalid dungeon
 
 --Auto Points Settings
 getgenv().AutoPointsToggle = true --false to disable
@@ -18,6 +18,8 @@ getgenv().distanceBetweenTp = 100
 getgenv().healthPercentageToKill = 0.5
 getgenv().tpLimit = 10 --Only tps 15 times maximum
 getgenv().OffsetVector = Vector3.new(0,0,-5) --Change to lock onto the exact enemy instead of offset
+getgenv().targetType = "furthest" --either random, nearest, furthest
+getgenv().tpToBosses = true --risky feature, high kick rate so disable if getting kicked on boss (but allows fast runs)
 
 --Auto Equipped Settings Settings
 getgenv().AutoEquip = true --set to false to disable auto equip
@@ -27,5 +29,6 @@ getgenv().LegsType = "health"
 getgenv().WeaponsType = "physicalPower"
 getgenv().AutoUpgrade = true --tries to upgrade each piece by 1 level in their respective stat
 --Note it only unequips after first run so auto equip/upgrade wont work on first ever run
+
 
 loadstring(game:HttpGet"https://raw.githubusercontent.com/Hexay/Fabled-Legacy/main/main/main.lua")()
